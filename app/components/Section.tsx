511efe6a1303b756af1463995617b34c1df3c670
rubbenix <rubenix02@gmail.com>
2026-02-18 15:09:15 +0100

    darkmode

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Section({ title, children }: { title: string; children: React.ReactNode }) {
   return (
     <section className="mb-10 w-full">
-      <h2 className="text-lg sm:text-xl font-bold text-sky-400 dark:text-blue-200 mb-4">
+      <h2 className="text-lg sm:text-xl font-bold text-sky-400 dark:text-sky-400 mb-4">
         {title}
       </h2>
       {children}
